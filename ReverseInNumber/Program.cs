5318a57f389bd88e3e1454c7bb6950034cb6a2b1
DavidZeplin@zTechOps.com <DavidZeplin@ztechops.com>
2021-07-12 12:33:38 -0400

    Add project files.

---
+++ ReverseInNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseInNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numbers to reverse");
+            int Num = int.Parse(Console.ReadLine());
+            int Rev = 0;
+
+            while (Num > 0)
+            {
+                int rem = Num % 10;
+                Rev = (Rev * 10) + rem;
+                Num = Num / 10;
+            }
+            Console.WriteLine("Here reverse number: {0}", Rev);
+            Console.ReadLine();
+        }
+    }
+}
